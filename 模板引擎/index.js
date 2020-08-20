b4d7db6d2a263da42eb7e6afc4c5e45d2c51497d
lvyou <376607092@qq.com>
2020-08-20 23:19:20 +0800

    模板

--- 模板引擎/index.js
+++ 模板引擎/index.js
@@ -1,10 +1,41 @@
-const user={
-    name:'yang'
-}
+const user = {
+	name: '<script />',
+};
+
+const result = `<h2>${user.name}</h2>`;
+const vm = require('vm');
+
+const templateMap = {
+	templateA: '`<h2>${include("templateB")}</h2>`',
+	templateB: '`<p>wwwwwww</p>`',
+};
+
+const context = {
+	include: function (name) {
+        console.log('name: ', name);
+		return templateMap[name]();
+	},
+	_: function (markup) {
+		if (!markup) return;
+		return String(markup)
+			.replace(/&/g, '&amp;')
+			.replace(/</g, '&lt;')
+			.replace(/>/g, '&gt;')
+			.replace(/'/g, '&#39;')
+			.replace(/"/g, '&quot;');
+	},
+};
+
+Object.keys(templateMap).forEach((key) => {
+    const temp = templateMap[key];
+    console.log('temp: ', temp);
+	templateMap[key] = vm.runInNewContext(`
+     (function(){
+         return ${temp}
+     })
+  `,context);
+});
 
 
 
-const result=`<h2>${user.name}</h2>`;
-const vm=require('vm');
-
-console.log(vm.runInNewContext('`<h2>${user.name}</h2>`',{user}));
+console.log(templateMap['templateA']());
